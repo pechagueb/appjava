2a11fc18fb4a50dec751cab7c151cfc2ff2ee8dc
Patricio Echague <pechagueb@gmail.com>
2025-08-25 11:51:00 +0100

    Prácticas

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,44 @@ public class Main {
     public static void main(String[] args) {
         //TIP Pulsa <shortcut actionId="ShowIntentionActions"/> con tu cursor en el texto resaltado
         // para ver cómo IntelliJ IDEA sugiere corregirlo.
-        System.out.printf("Hello and welcome! \n");
+/*
+    System.out.printf("Hello and welcome! \n");
 
-        for (int i = 1; i <= 5; i++) {
+    for (int i = 1; i <= 5; i++) {
             //TIP Presione <shortcut actionId="Debug"/> para iniciar la depuración de su código. Hemos creado un punto de interrupción <icon src="AllIcons.Debugger.Db_set_breakpoint"/>
             // para ti, pero siempre puedes añadir más pulsando <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    System.out.println("i = " + i);
+
         }
+
+
+ */
+
+        String saludar = "Hola, bienvenido";
+
+        System.out.println(saludar);
+
+        int numero = 11;
+        boolean valor = true;
+
+        if (valor){
+            System.out.println("El valor de 'número' es:" + numero);
+        }
+
+        int numero2 = 25;
+
+        String nombre = "";
+
+        if (numero2 < 26) {
+
+            nombre = "Juan";
+
+        }else{
+
+            nombre = "María";
+        }
+
+        System.out.println(nombre);
+
     }
 }
